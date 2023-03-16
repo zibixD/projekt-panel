f02f155c5c026d17bd9737794d550067c4f28334
Adrian Pląsek <adrian.plasek@shockit.pl>
2023-03-16 14:15:29 +0100

    Navigation bar

--- src/components/CompanyDetail.jsx
+++ src/components/CompanyDetail.jsx
@@ -1,3 +1,9 @@
+import { Card, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { Tabs, Tab} from "@mui/material";
+import { TabPanel, TabContext } from '@mui/lab'
+
+
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getAuthToken } from "../util/auth";
@@ -5,6 +11,7 @@ import { getAuthToken } from "../util/auth";
 const CompanyDetail = () => {
   const token = getAuthToken();
   const [details, setDetails] = useState(null);
+  const [value, setValue] = useState('1')
   const params = useParams();
 
   useEffect(() => {
@@ -15,12 +22,36 @@ const CompanyDetail = () => {
       .then((json) => setDetails(json));
   }, []);
 
+  const changeHandler = (event, newValue) => {
+    setValue(newValue);
+  }
+
   return (
     <>
-      <h1>Szczegóły firmy</h1>
-      <div style={{ height: 600, width: "100%" }}></div>
+      { details &&  <><Typography  sx={{ fontSize: 40}}>Szczegóły firmy - {details?.name ? details.name : "Nie podano"}</Typography>
+      <Tabs value={value} onChange={changeHandler}  aria-label="basic tabs example">
+        <Tab  label="Szczegóły firmy" />
+        <Tab label="Pracownicy" />
+      </Tabs>
+        <TabContext value={value}>
+        <Card sx={{ padding: 5}} > 
+            <TabPanel value="1">
+                <Typography variant="h5" >Nazwa firmy: {details?.name ? details.name : "Brak"}</Typography>
+                <Typography variant="body1">NIP: {details?.nip ? details.nip : "Brak"}</Typography>
+                <Typography variant="body1">Miasto: {details?.city ? details.city : "Brak"}</Typography>
+                <Typography variant="body1">Ulica: {details?.street ? details.street : "Brak"}</Typography>
+                <Typography variant="body1">Numer domu: {details?.houseNumber ? details.houseNumber : "Brak"}</Typography>
+                <Typography variant="body1">Numer lokalu: {details?.apartmentNumber ? details.apartmentNumber : "Brak"}</Typography>
+                <Typography variant="body1">Kod pocztowy: {details?.postalCode ? details.postalCode : "Brak"}</Typography>
+            </TabPanel>
+            <TabPanel value="2">
+                <Typography variant="h1">Elo</Typography>
+            </TabPanel>
+        </Card>
+        </TabContext></>}
     </>
   );
 };
 
 export default CompanyDetail;
+
