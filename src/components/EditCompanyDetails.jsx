1e35b096f884d5d74ac7722a3d3871859e87565c
nogisz-pr <dawid.mazurkiewicz@shockit.pl>
2023-03-27 12:54:09 +0200

    add confirm modal

--- src/components/EditCompanyDetails.jsx
+++ src/components/EditCompanyDetails.jsx
@@ -28,7 +28,7 @@ const schema = yup.object({
 const EditCompanyDetails = () => {
   const {
     register,
-    formState: { errors },
+    formState: { errors, isDirty },
     handleSubmit,
   } = useForm({
     resolver: yupResolver(schema),
@@ -36,15 +36,14 @@ const EditCompanyDetails = () => {
   const editDetails = useSelector((state) => state.company.details);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const isAuth = useIsAuthenticated()
-  const [isChanged, setIsChanged] = useState(false)
+  const isAuth = useIsAuthenticated();
+  const [canShowModal, setCanShowModal] = useState(false);
 
   useEffect(() => {
-    if(!isAuth) {
-      navigate("/")
+    if (!isAuth) {
+      navigate("/");
     }
-  }, [isAuth])
-  
+  }, [isAuth]);
 
   const onSubmit = async (data) => {
     await companyService
@@ -58,178 +57,187 @@ const EditCompanyDetails = () => {
       .catch(async () => {
         await dispatch(showErrorSnack("Wystąpił błąd podczas edycji firmy"));
       });
-    };
+  };
 
-    let editedData = editDetails;
-    // console.log(editedData)
-    const changeHandler = event => {
-      if(editedData !== event.target.value ) {
-        console.log("zmiana")
-      
-      } else if(editedData === event.target.value){
-        setIsChanged(true)
-        console.log(isC)
-        
-      }   
-      console.log(event.target.value)
-  }
-
+  useEffect(() => {
+    if (isDirty) {
+      setCanShowModal(true);
+    }
+  }, [isDirty]);
 
   return (
-    <Box sx={{
-      display: "flex",
-         flexDirection: "column",
-         alignItems: "center",
-      }}>
-      <Card sx={{
-        backgroundColor: "primary.light",
-        width: "100%",
-        color: "white",
-        textAlign: "center",
-        boxShadow: 5,
-        marginBottom: 0.3,
+    <Box
+      sx={{
         display: "flex",
-          alignItems: "center",
-          justifyContent: "center"
-      }}>
-       <Box>
-        <ConfirmSlide>
-          <HomeIcon/>
-        </ConfirmSlide>
-      </Box>
-      <Box sx={{
-          display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          width: "90%"
-         }}>
-        <Typography variant="h3" sx={{
-          height: 50,
-          padding: 4
-        }}>{editDetails.name}</Typography>
-        <Typography variant="subtitle1" sx={{
-          paddingBottom: 0.5
-        }}>edytuj dane firmy </Typography>
-      </Box>
-      </Card>
-      <Card sx={{
-         display: "flex",
-            flexWrap: "wrap",
-            flexDirection: "column",
-            alignItems: "center",
-        padding: 8,
-        boxShadow: 5,
-        width: { sx: "100%", sm: 600}
+        flexDirection: "column",
+        alignItems: "center",
       }}
-      >
-      
-      <form onChange={changeHandler} onSubmit={handleSubmit(onSubmit)}>
-        <Box>
-          <TextField
-            sx={{
-              mb: 5,
-              mt: 5,
-              mx: 5,
-              width: { sx: "100%", sm: 500 }
-            }}
-            label="Nazwa firmy"
-            name="name"
-            defaultValue={editDetails.name}
-            error={!!errors.name}
-            {...register("name")}
-          />
-        </Box>
-        <Box>
-          <TextField
-          sx={{
-            mb: 5,
-            mx: 5,
-            width: { sx: "100%", sm: 500 }
-          }}
-            label="NIP"
-            name="nip"
-            defaultValue={editDetails.nip}
-            error={!!errors.nip}
-            {...register("nip")}
-          />
-        </Box>
-        <Box>
-          <TextField
-          sx={{
-            mb: 5,
-            mx: 5,
-            width: { sx: "100%", sm: 500 }
-          }}
-            label="Miasto"
-            name="city"
-            defaultValue={editDetails.city}
-            error={!!errors.city}
-            {...register("city")}
-          />
-        </Box>
-        <Box>
-          <TextField
-          sx={{
-            mb: 5,
-            mx: 5,
-            width: { sx: "100%", sm: 500 }
-          }}
-            label="Ulica"
-            name="street"
-            defaultValue={editDetails.street}
-            error={!!errors.street}
-            {...register("street")}
-          />
-        </Box>
-        <Box>
-          <TextField
-          sx={{
-            mb: 5,
-            mx: 5,
-            width: { sx: "100%", sm: 500 }
-          }}
-            label="Numer domu"
-            name="houseNumber"
-            defaultValue={editDetails.houseNumber}
-            error={!!errors.houseNumber}
-            {...register("houseNumber")}
-          />
-        </Box>
-        <Box>
-          <TextField
-          sx={{
-            mb: 5,
-            mx: 5,
-            width: { sx: "100%", sm: 500 }
-          }}
-            label="Numer lokalu"
-            name="apartmentNumber"
-            defaultValue={editDetails.apartmentNumber}
-            error={!!errors.apartmentNumber}
-            {...register("apartmentNumber")}
-          />
-        </Box>
-        <Box>
-          <TextField
-          sx={{
-            mb: 4.3,
-            mx: 5,
-            width: { sx: "100%", sm: 500 }
-          }}
-            label="Kod pocztowy"
-            name="postalCode"
-            error={!!errors.postalCode}
-            defaultValue={editDetails.postalCode}
-            {...register("postalCode")}
-          />
-        </Box>
-        <Button type="submit"
+    >
+      <Card
         sx={{
-          height: 50,
-          width: { sx: "100%", sm: 500 },
-          mx: 5,
-        }}>Zapisz</Button>
-      </form>
+          backgroundColor: "primary.light",
+          width: "100%",
+          color: "white",
+          textAlign: "center",
+          boxShadow: 5,
+          marginBottom: 0.3,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box>
+          <ConfirmSlide canShowModal={canShowModal} />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "90%",
+          }}
+        >
+          <Typography
+            variant="h3"
+            sx={{
+              height: 50,
+              padding: 4,
+            }}
+          >
+            {editDetails.name}
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            sx={{
+              paddingBottom: 0.5,
+            }}
+          >
+            edytuj dane firmy
+          </Typography>
+        </Box>
+      </Card>
+      <Card
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: 8,
+          boxShadow: 5,
+          width: { sx: "100%", sm: 600 },
+        }}
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box>
+            <TextField
+              sx={{
+                mb: 5,
+                mt: 5,
+                mx: 5,
+                width: { sx: "100%", sm: 500 },
+              }}
+              label="Nazwa firmy"
+              name="name"
+              defaultValue={editDetails.name}
+              error={!!errors.name}
+              {...register("name")}
+            />
+          </Box>
+          <Box>
+            <TextField
+              sx={{
+                mb: 5,
+                mx: 5,
+                width: { sx: "100%", sm: 500 },
+              }}
+              label="NIP"
+              name="nip"
+              defaultValue={editDetails.nip}
+              error={!!errors.nip}
+              {...register("nip")}
+            />
+          </Box>
+          <Box>
+            <TextField
+              sx={{
+                mb: 5,
+                mx: 5,
+                width: { sx: "100%", sm: 500 },
+              }}
+              label="Miasto"
+              name="city"
+              defaultValue={editDetails.city}
+              error={!!errors.city}
+              {...register("city")}
+            />
+          </Box>
+          <Box>
+            <TextField
+              sx={{
+                mb: 5,
+                mx: 5,
+                width: { sx: "100%", sm: 500 },
+              }}
+              label="Ulica"
+              name="street"
+              defaultValue={editDetails.street}
+              error={!!errors.street}
+              {...register("street")}
+            />
+          </Box>
+          <Box>
+            <TextField
+              sx={{
+                mb: 5,
+                mx: 5,
+                width: { sx: "100%", sm: 500 },
+              }}
+              label="Numer domu"
+              name="houseNumber"
+              defaultValue={editDetails.houseNumber}
+              error={!!errors.houseNumber}
+              {...register("houseNumber")}
+            />
+          </Box>
+          <Box>
+            <TextField
+              sx={{
+                mb: 5,
+                mx: 5,
+                width: { sx: "100%", sm: 500 },
+              }}
+              label="Numer lokalu"
+              name="apartmentNumber"
+              defaultValue={editDetails.apartmentNumber}
+              error={!!errors.apartmentNumber}
+              {...register("apartmentNumber")}
+            />
+          </Box>
+          <Box>
+            <TextField
+              sx={{
+                mb: 4.3,
+                mx: 5,
+                width: { sx: "100%", sm: 500 },
+              }}
+              label="Kod pocztowy"
+              name="postalCode"
+              error={!!errors.postalCode}
+              defaultValue={editDetails.postalCode}
+              {...register("postalCode")}
+            />
+          </Box>
+          <Button
+            type="submit"
+            sx={{
+              height: 50,
+              width: { sx: "100%", sm: 500 },
+              mx: 5,
+            }}
+          >
+            Zapisz
+          </Button>
+        </form>
       </Card>
     </Box>
   );
